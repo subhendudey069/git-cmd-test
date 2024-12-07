31d043b7d8a75224199124eb729bfb143636a57c
Subhendu Dey <subhendudey069@gmail.com>
2024-12-07 17:12:10 +0530

    Revert "commit 2"
    This reverts commit 7a34b3208d2e847bc7cf89cb53d142905570cbe2.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 //1st commit
 console.log('Hello bhailogs keyse hoo')
 console.log('Newline at first commit');
-
-//2nd commit
-console.log('Kya hal chal')
-
-//3rd commit
-console.log('Kya hal chal')
